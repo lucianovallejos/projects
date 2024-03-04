f3a7eb31a869e1436fc05ccd6eea565a3a2ec94a
Luciano Vallejos <148650643+lucianovallejos@users.noreply.github.com>
2024-03-04 00:56:30 -0300

    Add files via upload

--- entrega final/routes/admin/novedades.js
+++ entrega final/routes/admin/novedades.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var usuariosModel = require('../../models/usuariosModel');
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get('/', function (req, res, next) {
+// para listar las novedades
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        usuario: req.session.nombre
+        usuario: req.session.nombre,
+        novedades
     });
 });
+
+// para eliminar novedades 
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades');
+
+}); // cierra get de eliminar
+
 
 module.exports = router;
--- entrega final/routes/novedades.js
+++ entrega final/routes/novedades.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('novedades',{isNovedades:true}); // novedades.hbs
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+
+  res.render('novedades', {
+    isNovedades: true,
+    novedades
+  });
 });
+
 
 module.exports = router;
